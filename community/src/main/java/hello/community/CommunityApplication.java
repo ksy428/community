0b26c8486ae3cf609662178276fdc4af22f13e8b
Seyoung Kim <tpdud0428@gmail.com>
2022-11-14 22:18:45 +0900

    member,post 기본구현

--- community/src/main/java/hello/community/CommunityApplication.java
+++ community/src/main/java/hello/community/CommunityApplication.java
@@ -2,12 +2,14 @@ package hello.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+
+@EnableJpaAuditing
+@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 public class CommunityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
-
 }
